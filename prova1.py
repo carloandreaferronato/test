3eedb144bbcacdf2900934dd7abaa930916d4b54
carloandreaferronato <113102646+carloandreaferronato@users.noreply.github.com>
2022-09-24 16:54:57 +0800

    initial commit

---
+++ prova1.py
@@ -0,0 +1,10 @@
+import sys
+sys.stdout.write("paolo sei un coglione\n")
+answer = sys.stdin.readline().strip()
+
+if answer =="y":
+    sys.stdout.write("we knew it\n")
+else:
+    sys.stdout.write("wtf is wrong wu\n")
+    
+
