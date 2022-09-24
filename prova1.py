507003e68dff702ec9cadc13e63dfb8f1016aa4e
carloandreaferronato <113102646+carloandreaferronato@users.noreply.github.com>
2022-09-24 17:03:23 +0800

    addfu

--- prova1.py
+++ prova1.py
@@ -4,6 +4,10 @@ answer = sys.stdin.readline().strip()
 
 if answer =="y":
     sys.stdout.write("we knew it\n")
+    sys.stdout.write("carlo is too\n")
+    carlo = sys.stdin.readline().strip()
+    if carlo =="y":
+       sys.stdout.write("we knew it\n")
 else:
     sys.stdout.write("wtf is wrong wu\n")
     
